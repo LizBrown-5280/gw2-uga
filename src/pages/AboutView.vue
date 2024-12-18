63ebf8fc717cd5b8cb21f3c1e2697a60664eb879
Liz Brown <liz.brown@entercom.com>
2024-12-17 18:43:11 -0700

    First commit

---
+++ src/pages/AboutView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <h1>About this site</h1>
+    <p>
+      I created this site as a place to work on my coding skills and thought it would be fun to work with Guild Wars 2
+      Web API since I am a long time GW player. I have started to published pages for this site, but I just want to be
+      clear that the GW2 section may never be fully realized. With that being said, the accuracy of the open data
+      provided and data retrieved by use of a GW2 API key will be as accurate as the GW2 source iteself.
+    </p>
+    <br />
+    <p>My practices and policy about your data is that:</p>
+    <ul>
+      <li>This site does not currently utilize cookies on your device.</li>
+      <li>If you choose to save your account keys, they will be stored in local storage on your device.</li>
+      <li>Should I decide to store any data on the server in the future, I will inform you here.</li>
+      <li>The only circumstance under which I would share your data is if compelled by a court order.</li>
+      <li>
+        I commit to never selling, trading, renting, or giving away any Guild Wars 2 API keys or data obtained through
+        those keys.
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+}
+</style>
