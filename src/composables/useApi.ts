eb2def5cfb4a1dbceef832c572c6d7e0db859c43
Liz Brown <liz.brown@entercom.com>
2024-12-27 20:08:05 -0700

    adding axois updating prettier

---
+++ src/composables/useApi.ts
@@ -0,0 +1,26 @@
+import axios from 'axios'
+import { ref } from 'vue'
+import type { IGw2Wallet } from '@/interfaces/Gw2Interfaces'
+
+axios.defaults.baseURL = 'https://api.guildwars2.com'
+
+export function useApi() {
+  const data = ref<IGw2Wallet[] | null>(null)
+  const error = ref(null)
+  const loading = ref(false)
+
+  const fetchData = async (url: string) => {
+    loading.value = true
+    error.value = null
+    try {
+      const response = await axios.get(url)
+      data.value = response.data
+    } catch (err) {
+      error.value = err
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return { data, error, loading, fetchData }
+}
